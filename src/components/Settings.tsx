cc5dc1fd6f46d210e34e501b6a89eb413517f7b7
Acquite <acquitefx@gmail.com>
2022-09-19 11:37:04 +0100

    ver 1.0.0 (dev - initial commit)

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,19 +1,71 @@
-import { FormRow, FormSwitch } from 'enmity/components';
-import { SettingsStore } from 'enmity/api/settings';
-import { React } from 'enmity/metro/common';
+import { FormDivider, FormRow, ScrollView, FormSwitch, Form, FormSection } from 'enmity/components';
+import { SettingsStore, getBoolean } from 'enmity/api/settings';
+import { getIDByName } from 'enmity/api/assets';
+import { React, Toasts, Constants, StyleSheet } from 'enmity/metro/common';
+import { version, release} from '../../manifest.json';
+import { bulk, filters} from 'enmity/metro';
 
 interface SettingsProps {
    settings: SettingsStore;
 }
 
+const [
+    Router,
+    Clipboard,
+ ] = bulk(
+    filters.byProps('transitionToGuild'),
+    filters.byProps('setString'),
+ );
+
 export default ({ settings }: SettingsProps) => {
-   return <FormRow
-      label='Example Setting'
-      trailing={
-         <FormSwitch
-            value={settings.get('example', true)}
-            onValueChange={() => settings.toggle('example', true)}
-         />
-      }
-   />;
+   const toastTrail = getIDByName('ic_selection_checked_24px');
+   const styles = StyleSheet.createThemedStyleSheet({
+        icon: {
+            color: Constants.ThemeColorMap.INTERACTIVE_NORMAL
+        }
+    });
+   return <>
+    <ScrollView>
+		<FormSection title="Disable Entire Plugin">
+            <FormRow
+                label='Disable Plugin'
+                leading={<FormRow.Icon style={styles.icon} source={getIDByName('ic_rulebook_16px')} />}
+                trailing={
+                    <FormSwitch
+                        value={settings.getBoolean('masterDisable', false)}
+                        onValueChange={() => {
+                                settings.toggle('masterDisable', false)
+                                Toasts.open({ content: `Successfully ${settings.getBoolean('masterDisable', false) ? 'disabled' : 'enabled'} AccountInfo.`, source: toastTrail });
+                            }
+                        }
+                    />
+                }
+            />
+        </FormSection>
+        <FormDivider />
+        <FormSection title="Source Code">
+            <FormRow
+                label="Download"
+                subLabel="Copy the link of the plugin to Clipboard"
+                leading={<FormRow.Icon style={styles.icon} source={getIDByName('toast_copy_link')} />}
+                trailing={FormRow.Arrow}
+                onPress={() => {
+                    Clipboard.setString("https://raw.githubusercontent.com/acquitelol/dislate/main/dist/Dislate.js");
+                    Toasts.open({ content: 'Copied to clipboard', source: getIDByName('pending-alert') });
+                }}
+            />
+            <FormRow
+                label="Source"
+                subLabel="Open the Repo of the Plugin Externally"
+                leading={<FormRow.Icon style={styles.icon} source={getIDByName('ic_leave_stage')} />}
+                trailing={FormRow.Arrow}
+                onPress={() => {
+                    Router.openURL("https://github.com/acquitelol/dislate")
+                }}
+            />
+        </FormSection>
+		<FormRow label={`Plugin Version: ${version}
+Release Channel: ${release}`} />
+    </ScrollView>
+   </>
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,82 @@
+// main imports of elements and dependencies
+import { Constants, React, StyleSheet } from 'enmity/metro/common';
+import { FormDivider, FormRow, Text, View} from 'enmity/components';
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
-import { React } from 'enmity/metro/common';
-import { getByProps } from 'enmity/metro';
+import { getIDByName } from 'enmity/api/assets';
+import { bulk, filters, getByProps } from 'enmity/metro';
 import { create } from 'enmity/patcher';
 import manifest from '../manifest.json';
-
 import Settings from './components/Settings';
+import { getBoolean, set, toggle } from 'enmity/api/settings'
+import { findInReactTree, findInTree } from 'enmity/utilities'
 
-const Typing = getByProps('startTyping');
-const Patcher = create('silent-typing');
+// main declaration of modules being altered by the plugin
+const [
+   Clipboard,
+   LazyActionSheet,
+   getLastSelectedChannelId
+] = bulk(
+   filters.byProps('setString'),
+   filters.byProps("openLazy", "hideActionSheet"),
+   filters.byProps("getLastSelectedChannelId")
+);
 
-const SilentTyping: Plugin = {
+// initialization
+const Patcher = create('dislate');
+
+const Dislate: Plugin = {
    ...manifest,
 
    onStart() {
-      Patcher.instead(Typing, 'startTyping', () => { });
-      Patcher.instead(Typing, 'stopTyping', () => { });
+      Patcher.before(LazyActionSheet, 'openLazy', (_, [component, sheet, message], res) => {
+         if (sheet !== 'MessageLongPressActionSheet') return res;
+         let channelId = getLastSelectedChannelId.getChannelId();
+
+         alert(channelId)
+
+         // setting booleans for toggling buttons
+         let masterDisableBool = getBoolean("AccountInfo", "masterDisable", false)
+
+         component.then(instance => {
+            Patcher.instead(instance, 'default', (self, message, orig) => {
+                  res.type.render = (...args) => {
+                     const res = orig.apply(self, args);
+                     const wrapper = findInTree(res, r => r.type?.render?.name === 'Wrapper', { walkable: ['props', 'type', 'children'] });
+                     if (!wrapper) return res;
+                     wrapper.type.render = (...args) => {
+                        const res = orig.apply(self, args);
+                        if (!res) return;
+                        const children = findInReactTree(res, r => r.find?.(c => Array.isArray(c)));
+                        if (!children || !children[1]) return res;
+                        const items = children[1];
+                        items.unshift(<FormRow
+                           label='Translate'
+                           leading={<FormRow.Icon source={getIDByName('img_nitro_star')} />}
+                           onPress={() => {
+                              let channelId = getLastSelectedChannelId.getChannelId();
+                              let content = message[0].message.content
+                              alert(`${channelId}
+${content}`)
+                              LazyActionSheet.hideActionSheet();
+                           }} />
+                        );
+                     return res;
+                  };
+                  return res;
+               };
+            })
+         })
+      });
    },
 
    onStop() {
+      // unpatches everything
       Patcher.unpatchAll();
    },
 
    getSettingsPanel({ settings }) {
       return <Settings settings={settings} />;
-   }
+   },
 };
 
-registerPlugin(SilentTyping);
+registerPlugin(Dislate);
