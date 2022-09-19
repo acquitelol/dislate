8148bc4712cc2bee75e00a4a8f92c687c0c72058
Acquite <acquitefx@gmail.com>
2022-09-19 14:53:30 +0100

    ver 1.2.2 (dev - wake up dispatcher)

--- src/index.tsx
+++ src/index.tsx
@@ -32,80 +32,94 @@ const Dislate: Plugin = {
    ...manifest,
 
    onStart() {
-      const unpatchActionSheet = Patcher.before(
-         LazyActionSheet,
-         "openLazy",
-         (_, [component, sheet], res) => {
-            
-            if (sheet === "MessageLongPressActionSheet") {
-               component.then((instance) => {
-                  Patcher.after(instance, "default", (_, message, res) => {
-                     const originalMessage = MessageStore.getMessage(
-                        message[0].message.channelId,
-                        message[0].message.id
-                     );
+      const unpatchActionSheet = () => {
 
-                     const origRender = res.type.render;
-                     res.type.render = function (...args) {
-                        const res = origRender.apply(this, args);
-                        const wrapper = findInTree(res, r => r.type?.render?.name === 'Wrapper', { walkable: ['props', 'type', 'children'] });
-                        if (!wrapper) return res;
-
-                        const origWrapper = wrapper.type.render;
-                        wrapper.type.render = function (...args) {
-                           const res = origWrapper.apply(this, args);
-                           if (!res) return;
-
-                           const children = findInReactTree(res, r => r.find?.(c => Array.isArray(c)));
-                           if (!children || !children[1]) return res;
-
-                           const items = children[1];
-                           items.unshift(<FormRow
-                              label='Translate'
-                              leading={<FormRow.Icon source={getIDByName('img_nitro_star')} />}
-                              onPress={() => {
-                                 // if (
-                                 //    !originalMessage?.editedTimestamp ||
-                                 //    originalMessage?.editedTimestamp._isValid
-                                 // ) {
-                                 //    try{
-                                 //       var origContent = originalMessage.content
-                                 //       var origChannel = originalMessage.channel_id
-                                 //    }catch{
-                                 //       var origContent = message[0].message.content
-                                 //       var origChannel = message[0].message.channel_id
-                                 //    }
-                                 //    message[0] = {};
-                                 //    const editEvent = {
-                                 //       type: "MESSAGE_UPDATE",
-                                 //       message: {
-                                 //          ...originalMessage,
-                                 //          edited_timestamp: "invalid_timestamp",
-                                 //          content:
-                                 //                origContent + ` \`[edited by ${manifest.authors[0].name}]\``,
-                                 //          guild_id: ChannelStore.getChannel(
-                                 //                origChannel
-                                 //          ).guild_id,
-                                 //       },
-                                 //       log_edit: false
-                                 //    };
-                                 //    FluxDispatcher.dispatch(editEvent);
-                                 // }
-
-                                 console.log(originalMessage)
-                                 LazyActionSheet.hideActionSheet()
-                                 items.shift()
-                              }} />
-                           );
-                           return res;
-                        };
-                     return res;
-                  };
-               });
-            });
+         for (const handler of ["MESSAGE_UPDATE", "MESSAGE_DELETE"]) {
+            try {
+               FluxDispatcher.dispatch({
+                     type: handler,
+                     message: {}, // should be enough to wake them up
+                  });
+            } catch (err) {
+               console.log('[Dislate Dispatch Error]', err);
+            }
          }
-      })
+      
+         Patcher.before(
+            LazyActionSheet,
+            "openLazy",
+            (_, [component, sheet], res) => {
+               
+               if (sheet === "MessageLongPressActionSheet") {
+                  component.then((instance) => {
+                     Patcher.after(instance, "default", (_, message, res) => {
+                        const originalMessage = MessageStore.getMessage(
+                           message[0].message.channelId,
+                           message[0].message.id
+                        );
 
+                        const origRender = res.type.render;
+                        res.type.render = function (...args) {
+                           const res = origRender.apply(this, args);
+                           const wrapper = findInTree(res, r => r.type?.render?.name === 'Wrapper', { walkable: ['props', 'type', 'children'] });
+                           if (!wrapper) return res;
+
+                           const origWrapper = wrapper.type.render;
+                           wrapper.type.render = function (...args) {
+                              const res = origWrapper.apply(this, args);
+                              if (!res) return;
+
+                              const children = findInReactTree(res, r => r.find?.(c => Array.isArray(c)));
+                              if (!children || !children[1]) return res;
+
+                              const items = children[1];
+                              items.unshift(<FormRow
+                                 label='Translate'
+                                 leading={<FormRow.Icon source={getIDByName('img_nitro_star')} />}
+                                 onPress={() => {
+                                    // if (
+                                    //    !originalMessage?.editedTimestamp ||
+                                    //    originalMessage?.editedTimestamp._isValid
+                                    // ) {
+                                    //    try{
+                                    //       var origContent = originalMessage.content
+                                    //       var origChannel = originalMessage.channel_id
+                                    //    }catch{
+                                    //       var origContent = message[0].message.content
+                                    //       var origChannel = message[0].message.channel_id
+                                    //    }
+                                    //    message[0] = {};
+                                    //    const editEvent = {
+                                    //       type: "MESSAGE_UPDATE",
+                                    //       message: {
+                                    //          ...originalMessage,
+                                    //          edited_timestamp: "invalid_timestamp",
+                                    //          content:
+                                    //                origContent + ` \`[edited by ${manifest.authors[0].name}]\``,
+                                    //          guild_id: ChannelStore.getChannel(
+                                    //                origChannel
+                                    //          ).guild_id,
+                                    //       },
+                                    //       log_edit: false
+                                    //    };
+                                    //    FluxDispatcher.dispatch(editEvent);
+                                    // }
+
+                                    console.log(originalMessage)
+                                    LazyActionSheet.hideActionSheet()
+                                    items.shift()
+                                 }} />
+                              );
+                              return res;
+                           };
+                        return res;
+                     };
+                  });
+               });
+            }
+            
+         })
+      }
       setTimeout(() => {
          unpatchActionSheet();
      }, 300); // give Flux some time to initialize -- 300ms should be more than enough
