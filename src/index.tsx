a45c41809bb5fa783fe036b361b62b317cc8a34b
Acquite <acquitefx@gmail.com>
2022-09-19 11:51:59 +0100

    ver 1.0.1 (dev - attempt to add extra item to lazyActionSheet)

--- src/index.tsx
+++ src/index.tsx
@@ -28,45 +28,31 @@ const Dislate: Plugin = {
    ...manifest,
 
    onStart() {
-      Patcher.before(LazyActionSheet, 'openLazy', (_, [component, sheet, message], res) => {
-         if (sheet !== 'MessageLongPressActionSheet') return res;
-         let channelId = getLastSelectedChannelId.getChannelId();
+      Patcher.before(
+         LazyActionSheet,
+         "openLazy",
+         (_, [component, sheet], res) => {
+           if (sheet === "MessageLongPressActionSheet") {
+             component.then((instance) => {
+               Patcher.after(instance, "default", (_, args, res) => {
+                  const children = findInReactTree(res, r => r.find?.(c => Array.isArray(c)));
+                  if (!children || !children[1]) return res;
+                  const items = children[1];
 
-         alert(channelId)
-
-         // setting booleans for toggling buttons
-         let masterDisableBool = getBoolean("AccountInfo", "masterDisable", false)
-
-         component.then(instance => {
-            Patcher.instead(instance, 'default', (self, message, orig) => {
-                  res.type.render = (...args) => {
-                     const res = orig.apply(self, args);
-                     const wrapper = findInTree(res, r => r.type?.render?.name === 'Wrapper', { walkable: ['props', 'type', 'children'] });
-                     if (!wrapper) return res;
-                     wrapper.type.render = (...args) => {
-                        const res = orig.apply(self, args);
-                        if (!res) return;
-                        const children = findInReactTree(res, r => r.find?.(c => Array.isArray(c)));
-                        if (!children || !children[1]) return res;
-                        const items = children[1];
-                        items.unshift(<FormRow
-                           label='Translate'
-                           leading={<FormRow.Icon source={getIDByName('img_nitro_star')} />}
-                           onPress={() => {
-                              let channelId = getLastSelectedChannelId.getChannelId();
-                              let content = message[0].message.content
-                              alert(`${channelId}
-${content}`)
-                              LazyActionSheet.hideActionSheet();
-                           }} />
-                        );
-                     return res;
-                  };
-                  return res;
-               };
-            })
-         })
-      });
+                  items.unshift(
+                     <FormRow
+                        iconSource={getIDByName("img_nitro_star")}
+                        label="Translate"
+                        onPress={() => {
+                           alert(`${args}`)
+                           LazyActionSheet.hideActionSheet();
+                        }}
+                     />
+                  );
+               });
+            });
+         }
+      })
    },
 
    onStop() {
