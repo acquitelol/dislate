2a476e333de554409765b311af0489ef54c5d5ec
Acquite <acquitefx@gmail.com>
2022-10-24 22:22:48 +0100

    ver 1.1.4 (ghost patch - add extra logs to /dislate debug

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,12 +1,14 @@
 import {name} from '../../manifest.json'
+import { Native } from 'enmity/metro/common'
+import { devices } from './devices'
 
 // returns an interpolated string lasting multiple lines of the debug info
 const debug_info = (version: string, release: string) => {
-    return `
-            **[${name}] Debug Information**
-> **Version:** ${version}
-> **Channel:** ${release}
-            `
+    return `**[${name}] Debug Information**
+> **Plugin Version:** ${version}
+> **Release Channel:** ${release}
+> **Software Version:** ${Native.DCDDeviceManager.systemVersion}
+> **Physical Device:** ${devices[Native.DCDDeviceManager.device]}`
 }
 
 export { debug_info }
