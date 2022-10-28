8db4a22d0e34688919354df91d55cc299f66cb11
Acquite <acquitefx@gmail.com>
2022-10-28 12:37:17 +0100

    ver 1.1.6 (stable - ghost patch - push comments out to finalize the function)

--- src/utils/update.ts
+++ src/utils/update.ts
@@ -1,3 +1,4 @@
+// Dependencies used in the script
 import { name, plugin, version } from "../../manifest.json";
 import { Dialog, REST, Toasts } from "enmity/metro/common";
 import { reload } from "enmity/api/native";
@@ -15,38 +16,57 @@ async function check_for_updates() {
     if (!external_version) return;
 
     // format the remote version
-    external_version = external_version[0].replaceAll('"', "");
+    external_version = external_version[0].replaceAll('"', ""); // make the string into a plain version (1.1.6 etc)
+
+    /* 
+    i dont need to match specific parts of the version, here are some tests~
+       -> version 1.1.6, latest is 1.1.9 (not the same so update)
+       -> version 1.1.7 latest is 1.3.2 (not the same so update)
+       -> version 1.2.5 latest is 1.2.5 (is the same so no update)
+    ~ theres no need to match for if the latest is higher than the current 
+      because the latest will always be larger or equal to the current version
+    */
+
+    // if the version is not the current one, that means its newer, otherwise run the no update function
     external_version != version ? show_update_dialog(url, external_version) : no_updates(name, version)
-    return
+    return // finish the function
 }
 
 const show_update_dialog = (url: string, version: string) => {
+    // open a dialog to show that a new version is available
     Dialog.show({
         title: "Update found",
-        body: `Update has been found for ${name}.
+        body: `A newer version is available for ${name}.
 Would you like to install version ${version} now?`,
         confirmText: "Update",
         cancelText: "Not now",
+        
+        // run the install function
         onConfirm: () => install_plugin(url, version),
     });
 }
 
 const no_updates = (name: string, version: string) => {
-    console.log(`${name} is on the latest version, which is version ${version}`)
+    // logs the fact that youre on the latest version with both a toast a
+    console.log(`[${name}] Plugin is on the latest version, which is version ${version}`)
     Toasts.open({ content: `${name} is on latest version (${version})`, source: Icons.Clipboard });
 }
 
 async function install_plugin(url: string, version: string) {
     //@ts-ignore
     window.enmity.plugins.installPlugin(url, ({ data }) => {
+        // as a callback, waits for a success of "installed-plugin" or "overriden-plugin"
+        // before showing the dialog to reload discord
         data=="installed-plugin" || data=="overriden-plugin" ? Dialog.show({
             title: `Updated ${name}`,
             body: `Successfully updated to version ${version}. 
 Would you like to reload Discord now?`,
             confirmText: "Reload",
             cancelText: "Not now",
+            // reload discord from native function
             onConfirm: () => {reload()},
         }) : console.log(`[Dislate] Plugin failed to update to version ${version}.`)
+        // otherwise log an issue when updating to console ^^^
     })
 }
 
