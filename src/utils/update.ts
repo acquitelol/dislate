8e64c2ac2c5727e3c26585b54ab5680646a8e488
Acquite <acquitefx@gmail.com>
2022-10-30 01:24:18 +0100

    ver 1.1.62 (stable - fix regex to match versions with subversions (eg 1.1.61 or 1.2.34)

--- src/utils/update.ts
+++ src/utils/update.ts
@@ -12,7 +12,7 @@ async function check_for_updates() {
     const content = await res.text;
 
     // get the version from the source
-    let external_version = content.match(/[0-9].[0-9].[0-9]/g);
+    let external_version = content.match(/^(\d+\.)?(\d+\.)?(\*|\d+)$/g);
     if (!external_version) return;
 
     // format the remote version
