efcef4dd9b00bc863e31bc2dd7b29c3018d0e19e
Acquite <acquitefx@gmail.com>
2023-01-16 21:07:22 +0000

    Ver 1.3.4 ~ Build 2.0.1 Minify the build again. I forgot to revert this after my testing was complete :3

--- rollup.config.ts
+++ rollup.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
       common_js(),
       json(),
       esbuild({ 
-         // minify: manifest.release=="stable", 
-         minify: false,
+         minify: manifest.release=="stable", 
+         // minify: false,
          target: 'ES2019',
       }),
    ]
